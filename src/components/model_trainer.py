04b0023062964cb22ed2e76a578ac0a88f66146a
Bhushan Malusare <bhushan.malusare02@gmail.com>
2023-06-09 13:02:00 +0530

    app.py created

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -99,7 +99,8 @@ class ModelTrainer:
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            else:
+                logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
